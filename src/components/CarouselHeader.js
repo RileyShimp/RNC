8522188788213c432ea9d2b4319c55fb6ae07930
RileyShimp <rilryshimp@gmail.com>
2022-06-16 22:35:35 -0700

    Add images for carousel and styling

--- src/components/CarouselHeader.js
+++ src/components/CarouselHeader.js
@@ -1,53 +1,51 @@
 import Carousel from 'react-bootstrap/Carousel'
-// import image1 from '../assets/img/headerPhoto1.jpeg'
-// import image2 from '../assets/img/headerPhoto2.jpeg'
-// import image3 from '../assets/img/headerPhoto3.jpeg'
+import image1 from '../assets/img/headerImage1.jpg'
+import image2 from '../assets/img/headerImage2.jpg'
+import image3 from '../assets/img/headerImage3.jpg'
 
 export default function CarouselHeader() {
   return (
     <div className='Carousel'>
       <Carousel style={{ color: '#14213d', margin: '0 auto' }}>
               <Carousel.Item>
-                {/* <img className='d-block w-100' src={image2} alt='Third slide' /> */}
+                <img className='d-block w-100' src={image2} alt='Third slide' />
       
                 <Carousel.Caption
                   style={{
-                    top: '0',
                     fontWeight: '700',
                     fontSize: '4vw',
-                    color: '#8ecae6',
+                    color: 'white',
                     textShadow: '2px 2px #023047',
                   }}>
-                  <h3 style={{ fontWeight: '700', fontSize: '6vw' }}>We Got Studs!</h3>
-                  <p>a variety of studs available</p>
+                  <h3 style={{ fontWeight: '700', fontSize: '6vw' }}>Reality PDX</h3>
+                  <p>Portlands Largest Night Club</p>
                 </Carousel.Caption>
               </Carousel.Item>
         <Carousel.Item>
-          {/* <img className='d-block w-100' src={image3} alt='Second slide' /> */}
+          <img className='d-block w-100' src={image3} alt='Second slide' />
 
           <Carousel.Caption
             style={{
-              top: '0',
               fontWeight: '700',
-              fontSize: '4vw',
-              color: '#8ecae6',
+              fontSize: '3vw',
+              color: 'white',
               textShadow: '2px 2px #023047',
             }}>
-            <h3 style={{ fontWeight: '700', fontSize: '6vw' }}>We Got Puppies!</h3>
-            <p>Contact us if you interested in adopting! </p>
+            <h3 style={{ fontWeight: '700', fontSize: '6vw' }}>VIP and Bottle Service</h3>
+            <p>Make reservations tonight! </p>
           </Carousel.Caption>
         </Carousel.Item>
             <Carousel.Item>
-              {/* <img className='d-block w-100' src={image1} alt='First slide' /> */}
+              <img className='d-block w-100' src={image1} alt='First slide' />
               <Carousel.Caption
                 style={{
                   fontWeight: '700',
                   fontSize: '4vw',
-                  color: '#8ecae6',
+                  color: 'white',
                   textShadow: '2px 2px #023047',
                 }}>
-                <h3 style={{ fontWeight: '700', fontSize: '7vw' }}>Wordwide Bullies</h3>
-                <p>AKC registered puppies and studs</p>
+                <h3 style={{ fontWeight: '700', fontSize: '7vw' }}>Never Miss Out</h3>
+                <p>Check our upcoming events!</p>
               </Carousel.Caption>
             </Carousel.Item>
       </Carousel>
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@
 export default function About() {
   return (
     <div>
-      <h1 style={{ textAlign: 'center', fontSize: '9vw', textShadow: '2px 2px white' }}>About us</h1>
+      <h1 style={{ textAlign: 'center', fontSize: '9vw' }}>About us</h1>
     </div>
   )
 }
